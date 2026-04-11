361a4fbfbe8537545d08cb0c2d747f95956c9250
gab <gabrilosgaitanidis@gmail.com>
2026-04-11 19:03:07 +0300

    Enabled cors on vercel url

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -17,6 +17,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:3002",
+      "https://api-generator-eight.vercel.app",
     ],
     credentials: true,
   }),
